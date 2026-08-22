f6b0414a2adfed243db9e61c1b9b2ecb9f537285
binishaa Pdl <binishapoudel18@gmail.com>
2026-08-22 11:03:30 +0545

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const bookmarkRoutes = require("./routes/bookmarkRoutes");
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://skyy.onrender.com',
   credentials: true
 }));
 app.use(express.json());
