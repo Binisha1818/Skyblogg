f38470718e2238ea69f88d982b6e87b8dc9eb271
binishaa Pdl <binishapoudel18@gmail.com>
2026-08-22 09:40:46 +0545

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { register, login, getProfile, forgotPassword, resetPassword, googleAuth } = require('../controllers/authControllers');
+const { register, login, getProfile, forgotPassword, resetPassword, googleAuth } = require('../controllers/authController');
 
 const protect = require('../middleware/authMiddleware');
 const { registerRules, loginRules, validate } = require('../middleware/validators');
