1ff71221720255a304e7c741121eed711ce926e5
binishaa Pdl <binishapoudel18@gmail.com>
2026-08-23 07:23:17 +0545

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,82 +1,148 @@
-import { useState } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useState } from "react";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import Opportunities from "./pages/opportunity";
+import PostOpportunity from "./pages/CreatePost";
+import { AuthProvider } from "./context/AuthContext";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
-import { AuthProvider } from './context/AuthContext';
-import Navbar from './components/Navbar';
-import Dashboard from './pages/Dashboard';
-import ProtectedRoute from './components/ProtectedRoute';
-import WritePost from './pages/WritePost';
-import PostView from './pages/PostView';
-import BlogList from './pages/BlogList';
-import EditPost from './pages/EditPost';
-import AdminDashboard from './pages/AdminDashboard';
+import Navbar from "./components/Navbar";
+import ProtectedRoute from "./components/ProtectedRoute";
+
+import LandingPage from "./pages/LandingPage";
+import BlogList from "./pages/BlogList";
+import Dashboard from "./pages/Dashboard";
+import WritePost from "./pages/WritePost";
+import PostView from "./pages/PostView";
+import EditPost from "./pages/EditPost";
+import AdminDashboard from "./pages/AdminDashboard";
 import Bookmark from "./pages/Bookmark";
 import ForgotPassword from "./pages/ForgotPassword";
 import ResetPassword from "./pages/ResetPassword";
-import { GoogleOAuthProvider } from '@react-oauth/google';
-import LandingPage from "./pages/LandingPage";
+
+
+function AppContent() {
+  const [search, setSearch] = useState("");
+  const location = useLocation();
+
+  // Hide the existing Navbar only on the Landing Page
+  const showNavbar = location.pathname !== "/";
+
+  return (
+    <>
+
+      {showNavbar && (
+        <Navbar
+          search={search}
+          setSearch={setSearch}
+        />
+      )}
+
+      <Routes>
+        {/* Opportunities */}
+  <Route path="/opportunity" element={<Opportunities />} />
+
+  {/* Post Opportunity */}
+  <Route
+    path="/CreatePost"
+    element={
+      <ProtectedRoute>
+        <PostOpportunity />
+      </ProtectedRoute>
+    }
+  />
+        {/* Landing Page */}
+        <Route path="/" element={<LandingPage />} />
+
+        {/* Existing Blog Feed */}
+        <Route
+          path="/home"
+          element={<BlogList search={search} />}
+        />
+
+        {/* Keep /blog working */}
+        <Route
+          path="/blog"
+          element={<BlogList search={search} />}
+        />
+
+        {/* Blog Details */}
+        <Route path="/blog/:id" element={<PostView />} />
+
+        {/* Forgot Password */}
+        <Route
+          path="/forgot-password"
+          element={<ForgotPassword />}
+        />
+
+        {/* Reset Password */}
+        <Route
+          path="/reset-password/:token"
+          element={<ResetPassword />}
+        />
+
+        {/* Dashboard */}
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* Bookmarks */}
+        <Route
+          path="/bookmark"
+          element={<Bookmark />}
+        />
+
+        {/* Write Post */}
+        <Route
+          path="/write"
+          element={
+            <ProtectedRoute>
+              <WritePost />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* Admin */}
+        <Route
+          path="/admin"
+          element={
+            <ProtectedRoute>
+              <AdminDashboard />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* Edit Post */}
+        <Route
+          path="/posts/:id/edit"
+          element={
+            <ProtectedRoute>
+              <EditPost />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* Post Details */}
+        <Route
+          path="/posts/:id"
+          element={<PostView />}
+        />
+      </Routes>
+    </>
+  );
+}
+
 
 function App() {
-  const [search, setSearch] = useState('');
-
-return (
+  return (
     <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
       <AuthProvider>
         <BrowserRouter>
-
-          <Navbar
-            search={search}
-            setSearch={setSearch}
-          />
-
-          <Routes>
-               <Route path="/" element={<LandingPage />} />
-            <Route path="/" element={<BlogList search={search} />} />
-            <Route path="/blog" element={<BlogList search={search} />} />
-            <Route path="/blog/:id" element={<PostView />} />
-            <Route path="/forgot-password" element={<ForgotPassword />} />
-            <Route path="/reset-password/:token" element={<ResetPassword />} />
-
-            <Route
-              path="/dashboard"
-              element={
-                <ProtectedRoute>
-                  <Dashboard />
-                </ProtectedRoute>
-              }
-            />
-            <Route path="/bookmark" element={<Bookmark />} />
-
-            <Route
-              path="/write"
-              element={
-                <ProtectedRoute>
-                  <WritePost />
-                </ProtectedRoute>
-              }
-            />
-
-            <Route
-              path="/admin"
-              element={
-                <ProtectedRoute>
-                  <AdminDashboard />
-                </ProtectedRoute>
-              }
-            />
-
-            <Route
-              path="/posts/:id/edit"
-              element={
-                <ProtectedRoute>
-                  <EditPost />
-                </ProtectedRoute>
-              }
-            />
-
-            <Route path="/posts/:id" element={<PostView />} />
-          </Routes>
-
+          <AppContent />
         </BrowserRouter>
       </AuthProvider>
     </GoogleOAuthProvider>
